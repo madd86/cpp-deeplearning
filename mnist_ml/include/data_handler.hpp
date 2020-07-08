f2da24da2c8e5bf2b9202f053f8d2498368bbff6
Gabriel Leon de Mattos <Gasd>
2020-07-08 10:00:16 -0400

    Added header

--- mnist_ml/include/data_handler.hpp
+++ mnist_ml/include/data_handler.hpp
@@ -2,7 +2,52 @@
 // Created by Gabriel Leon de Matos on 7/8/20.
 //
 
-#ifndef CPP_DEEPLEARNING_DATA_HANDLER_HPP
-#define CPP_DEEPLEARNING_DATA_HANDLER_HPP
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
 
-#endif //CPP_DEEPLEARNING_DATA_HANDLER_HPP
+#include "fstream"
+#include "stdint.h"
+#include "data.hpp"
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_set>
+#include <math.h>
+
+class DataHandler {
+    std::vector<Data *> *dataArray;
+    std::vector<Data *> *trainingData;
+    std::vector<Data *> *validationData;
+    int class_counts;
+    int featureVectorSize;
+    std::map<uint8_t, int> classFromInt;
+    std::map<std::string, int> classFromString;
+
+public:
+    const double TRAIN_SET_PERCENT = .1;
+    const double TEST_SET_PERCENT = 0.75;
+    const double VALID_SET_PERCENT = 0.005;
+
+    void readCsv(std::string, std::string);
+    void readInputData(std::string path);
+    void readLabelData(std::string path);
+    void splitData();
+    void countClasses();
+    void normalize();
+    void print();
+
+    int getClassCounts();
+    int getDataArraySize();
+    int getTrainingDataSize();
+    int getTestDataSize();
+    int getValidationSize();
+
+    uint32_t format(const unsigned char* bytes);
+
+    std::vector<Data *> * getTrainingData();
+    std::vector<Data *> * getTestData();
+    std::vector<Data *> * getValidationData();
+    std::map<uint8_t, int> getClassMap();
+};
+
+#endif
